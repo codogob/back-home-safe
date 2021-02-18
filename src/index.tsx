8c1d58382f8180d03666576dd203fd4cc5986d16
owjYzWcyixZcWUWJVwTf <6c7jntrkbrxajjzw8nwvlryz@protonmail.com>
2021-02-18 22:19:54 +0800

    chore: adding prettier for code formatting to reduce noise

--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-

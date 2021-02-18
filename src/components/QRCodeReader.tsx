c4c0d3379897fc142a6d58ba736743864e19e783
_ <_@_>
2021-02-19 01:17:15 +0800

    chore: remove unused dependency

--- src/components/QRCodeReader.tsx
+++ src/components/QRCodeReader.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import jsQR, { QRCode } from "jsqr";
 import styled from "styled-components";
 import { useRafLoop } from "react-use";

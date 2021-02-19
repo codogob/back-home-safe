591a22008d911531f8d0a01f6cb55eebc0673310
_ <_@_>
2021-02-19 19:43:09 +0800

    feat: change font

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ html {
 
 body {
   margin: 0;
-  font-family: Noto Sans, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  font-family: Rubik, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -77,7 +77,7 @@ body {
 }
 
 textarea{
-  font-family: Noto Sans, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  font-family: Rubik, -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
 }

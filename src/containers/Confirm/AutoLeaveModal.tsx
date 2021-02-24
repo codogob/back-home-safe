d61cf1908dcc6eee731e6aabb78e84c84c121592
thc 282 <thc282@gmail.com>
2021-02-24 14:43:18 +0000

    Update AutoLeaveModal.tsx

--- src/containers/Confirm/AutoLeaveModal.tsx
+++ src/containers/Confirm/AutoLeaveModal.tsx
@@ -77,7 +77,7 @@ export const AutoLeaveModal = ({
       <Title>設罝自動離開時間</Title>
       <HourListWrapper id="scroll">
         <HourList>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((hour) => (
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24].map((hour) => (
             <HourListItem
               onClick={() => {
                 setAutoLeaveHourTmp(hour);

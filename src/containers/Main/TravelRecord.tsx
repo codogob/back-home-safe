042eb0c4c30ca21572c59f20ba6a4337547088d4
_ <_@_>
2021-02-27 04:03:39 +0800

    chore: remove unused import

--- src/containers/Main/TravelRecord.tsx
+++ src/containers/Main/TravelRecord.tsx
@@ -9,7 +9,6 @@ import {
 
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { useTravelRecord } from "../../hooks/useTravelRecord";
 import { getVenueName } from "../../utils/qr";
